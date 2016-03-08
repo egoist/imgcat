33c816fbf2297a7e6d5a8b7bb454d6d4f78ed89b
EGOIST <0x142857@gmail.com>
2016-03-08 21:19:30 +0800

    detect iterm and its version, fixed #2

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var fs = require('fs')
-var fsPromise = require('fs-promise')
+var isIterm = require('is-iterm')
+var itermVersion = require('iterm-version')
+var pify = require('pify')
+var Promise = require('pinkie-promise')
 
 function base64(content) {
 	return new Buffer(content).toString('base64')
@@ -10,30 +13,43 @@ function meta(content, width) {
 	return '\033]1337;File=;inline=1;width=' + width + 'px:' + content + '\07\n'
 }
 
-function imgcat(img, width, cb) {
-	cb = typeof width === 'function' ? width : cb
-	width = typeof width === 'number' ? width : 400
-	if (cb) {
-		fs.readFile(img, function (err, content) {
-			if (err) {
-				return cb(err)
-			}
-			content = meta(base64(content), width)
-			cb(null, content)
-		})
-	} else {
-		return fsPromise.readFile(img)
+function imgcat(img, width) {
+	width = width || 400
+	if (!isIterm) {
+		return Promise.reject(new Error('You are not running in Iterm2!'))
+	}
+
+	function resolveImg() {
+		return pify(fs, Promise)
+			.readFile(img)
 			.then(function (content) {
 				content = base64(content)
 				return meta(content, width)
 			})
 	}
+
+	return itermVersion()
+		.then(function (version) {
+			if (!validItermVersion(version)) {
+				throw new Error('Expected iTerm vesion >= 2.9')
+			}
+		})
+		.then(resolveImg)
 }
 
 function sync(img, width) {
+	var version = itermVersion.sync()
+	if (!validItermVersion(version)) {
+		throw new Error('Expected iTerm vesion >= 2.9')
+	}
 	var content = fs.readFileSync(img)
 	content = base64(content)
 	return meta(content, width)
+}
+
+function validItermVersion(version) {
+	version = version.split('.')
+	return version[0] > 1 && version[1] > 8
 }
 
 
--- test.js
+++ test.js
@@ -1,12 +1,4 @@
 var imgcat = require('./')
-// async
-imgcat('media/imgcat.png', function (err, content) {
-	if (err) {
-		return console.log(err)
-	}
-	console.log('async')
-	console.log(content)
-})
 // sync
 console.log('sync')
 console.log(imgcat.sync('media/imgcat.png'))
@@ -15,4 +7,7 @@ imgcat('media/imgcat.png', 500)
 	.then(function (image) {
 		console.log('promise')
 		console.log(image)
+	})
+	.catch(function (e) {
+		console.log(e.stack)
 	})
